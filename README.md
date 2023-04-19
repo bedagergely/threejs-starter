56362c063bf856a8785a3eecacd46f3d3c98930d
bedagergely <47953606+bedagergely@users.noreply.github.com>
2023-04-19 21:14:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# threejs-starter
+npm run dev
+npm run build
---
+++ main.js
@@ -0,0 +1,31 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	// required if controls.enableDamping or controls.autoRotate are set to true
+	controls.update();
+
+	renderer.render( scene, camera );
+
+}
+
+animate();
